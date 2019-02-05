403cdd78f771c3c35120dbbc98281589e2e03887
DPigeon <D_Pig@encs.concordia.ca>
2019-02-05 00:30:33 -0500

    ISSUE9-AddTouching
    Touch was added by touching the screen to create more objects.

--- Components/Navigation/Modules/Play.js
+++ Components/Navigation/Modules/Play.js
@@ -30,6 +30,32 @@ const Physics = (entities, { time }) => {
   return entities;
 };
 
+/* Touching to Create Boxes*/
+let boxIds = 0;
+const CreateBox = (entities, { touches, screen }) => {
+  let world = entities["physics"].world;
+  let boxSize = Math.trunc(Math.max(screen.width, screen.height) * 0.075);
+  touches
+    .filter(t => t.type === "press")
+    .forEach(t => {
+      let body = Matter.Bodies.rectangle(
+        t.event.pageX,
+        t.event.pageY,
+        boxSize,
+        boxSize,
+        { frictionAir: 0.021, restitution: 1.0 } // Restitution: defines elasticity (bouncing)
+      );
+      Matter.World.add(world, [body]);
+      entities[++boxIds] = {
+        body: body,
+        size: [boxSize, boxSize],
+        color: boxIds % 2 == 0 ? "pink" : "#B8E986",
+        renderer: Box
+      };
+    });
+  return entities;
+};
+
 export default class Play extends React.Component {
   static navigationOptions = {
     header: null
@@ -38,7 +64,7 @@ export default class Play extends React.Component {
     return (
       <GameEngine
         style={styles.container}
-        systems={[Physics]}
+        systems={[Physics, CreateBox]}
         entities={{
           physics: {
             engine: engine,
