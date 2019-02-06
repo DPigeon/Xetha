b87f19d024fbbe59924870bea600e36811fb31e2
DPigeon <D_Pig@encs.concordia.ca>
2019-02-05 23:06:59 -0500

    Initialized the gameplay
    The constructor starts with an array.

--- Components/Navigation/Modules/GamePlay.js
+++ Components/Navigation/Modules/GamePlay.js
@@ -3,6 +3,26 @@ import { Text, View, StyleSheet } from "react-native";
 import { MaterialCommunityIcons as Icon } from "react-native-vector-icons";
 
 class GamePlay extends Component {
+  static navigationOptions = {
+    title: "Tic Tac Toe"
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      gameState: [], // The board this.state
+      currentPlayer: 1 // Player 1: 1, Player 2: -1
+    };
+  }
+
+  componentDidMount() {
+    this.initializeGame(); // Begin game
+  }
+
+  initializeGame = () => {
+    this.setState({ gameState: [[0, 0, 0], [0, 0, 0], [0, 0, 0]] });
+  };
+
   render() {
     return (
       <View style={styles.container}>
@@ -13,17 +33,21 @@ class GamePlay extends Component {
           >
             <Icon name="close" style={styles.tileX} />
           </View>
-          <View style={[styles.tile, { borderTopWidth: 0 }]} />
+          <View style={[styles.tile, { borderTopWidth: 0 }]}>
+            <Icon name="circle-outline" style={styles.tileO} />
+          </View>
           <View
             style={[styles.tile, { borderTopWidth: 0, borderRightWidth: 0 }]}
           />
         </View>
+
         {/* Row 2 */}
         <View style={{ flexDirection: "row" }}>
           <View style={[styles.tile, { borderLeftWidth: 0 }]} />
           <View style={[styles.tile, {}]} />
           <View style={[styles.tile, { borderRightWidth: 0 }]} />
         </View>
+
         {/* Row 3 */}
         <View style={{ flexDirection: "row" }}>
           <View
@@ -49,8 +73,16 @@ const styles = StyleSheet.create({
     justifyContent: "center"
   },
   tile: {
-    borderWidth: 1,
+    borderWidth: 5,
     width: 100,
     height: 100
+  },
+  tileX: {
+    color: "red",
+    fontSize: 60
+  },
+  tileO: {
+    color: "lime",
+    fontSize: 60
   }
 });
