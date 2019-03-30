c34113519b837152319639fa568ba2482cefacb3
DPigeon <D_Pig@encs.concordia.ca>
2019-03-30 13:46:57 -0400

    Refactored Interface StyleSheet

--- Components/OtherGame/GameInterface.js
+++ Components/OtherGame/GameInterface.js
@@ -1,16 +1,11 @@
 import React, { Component } from "react";
-import {
-  Text,
-  View,
-  StyleSheet,
-  TouchableOpacity,
-  Dimensions
-} from "react-native";
+import { Text, View, TouchableOpacity, Dimensions } from "react-native";
 import { MaterialCommunityIcons as Icon } from "react-native-vector-icons";
 import PointsButton from "./PointsButton";
 import SnowflakesAnimated from "./Effects/Snowflakes/Animated";
 import DuckMinigame from "./Effects/Duck/MinigameModal";
 import SnowmanMinigame from "./Effects/Snowman/SnowmanModal";
+import { styles } from "./StyleSheetInterface";
 
 const { height, width } = Dimensions.get("window");
 
@@ -264,71 +259,3 @@ class GameInterface extends Component {
 }
 
 export default GameInterface;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  tile: {
-    borderWidth: 1,
-    width: 65,
-    height: 65
-  },
-  tileGhost: {
-    color: "brown",
-    fontSize: 60
-  },
-  tileDuck: {
-    color: "purple",
-    fontSize: 60
-  },
-  tileSnowman: {
-    color: "blue",
-    fontSize: 60
-  },
-  tileMultitouch: {
-    color: "gray",
-    fontSize: 60
-  },
-  tileTarget: {
-    color: "red",
-    fontSize: 60
-  },
-  tileSnowflake: {
-    color: "cyan",
-    fontSize: 60
-  },
-  tileEnergy: {
-    color: "red",
-    fontSize: 60
-  },
-  tileWii: {
-    color: "lime",
-    fontSize: 60
-  },
-  Button: {
-    backgroundColor: "#2d594d",
-    borderColor: "white",
-    borderWidth: 1,
-    borderRadius: 50,
-    color: "white",
-    fontSize: 17,
-    fontWeight: "bold",
-    overflow: "hidden",
-    padding: 12,
-    textAlign: "center"
-  },
-  ButtonPosition: {
-    bottom: -123,
-    right: 0
-  },
-  minigameContainer: {
-    position: "absolute",
-    top: width / 3,
-    width: width,
-    height: height / 2
-  }
-});
---
+++ Components/OtherGame/StyleSheetInterface.js
@@ -0,0 +1,71 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  tile: {
+    borderWidth: 1,
+    width: 65,
+    height: 65
+  },
+  tileGhost: {
+    color: "brown",
+    fontSize: 60
+  },
+  tileDuck: {
+    color: "purple",
+    fontSize: 60
+  },
+  tileSnowman: {
+    color: "blue",
+    fontSize: 60
+  },
+  tileMultitouch: {
+    color: "gray",
+    fontSize: 60
+  },
+  tileTarget: {
+    color: "red",
+    fontSize: 60
+  },
+  tileSnowflake: {
+    color: "cyan",
+    fontSize: 60
+  },
+  tileEnergy: {
+    color: "red",
+    fontSize: 60
+  },
+  tileWii: {
+    color: "lime",
+    fontSize: 60
+  },
+  Button: {
+    backgroundColor: "#2d594d",
+    borderColor: "white",
+    borderWidth: 1,
+    borderRadius: 50,
+    color: "white",
+    fontSize: 17,
+    fontWeight: "bold",
+    overflow: "hidden",
+    padding: 12,
+    textAlign: "center"
+  },
+  ButtonPosition: {
+    bottom: -123,
+    right: 0
+  },
+  minigameContainer: {
+    position: "absolute",
+    top: width / 3,
+    width: width,
+    height: height / 2
+  }
+});
+
+export default styles;
